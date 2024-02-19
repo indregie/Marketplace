094ffe25cc84f64c8aafb4194c7cfdd8f05e112d
indregie <91377174+indregie@users.noreply.github.com>
2024-02-19 17:43:45 +0200

    Order service unit test added

--- tests/Services/OrderServiceTests.cs
+++ tests/Services/OrderServiceTests.cs
@@ -1,37 +1,66 @@
+using Application.Interfaces;
+using Application.Services;
+using AutoFixture;
+using AutoFixture.Xunit2;
+using Domain.Dtos.Request;
+using Domain.Dtos.Response;
+using Domain.Entities;
+using Domain.Interfaces;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
 namespace UnitTests.Services;
 
 public class OrderServiceTests
 {
-    //private readonly Mock<IRateRepository> _rateRepositoryMock;
-    //private readonly Mock<IExchangeRatesClient> _rateClientMock;
-    //private readonly RateService _rateService;
-    //private readonly Fixture _fixture;
-    //public RateServiceTests()
-    //{
-    //    _rateRepositoryMock = new Mock<IRateRepository>();
-    //    _rateClientMock = new Mock<IExchangeRatesClient>();
-    //    _rateService = new RateService(_rateClientMock.Object, _rateRepositoryMock.Object);
-    //    _fixture = new Fixture();
-    //}
+    private readonly Mock<IOrderRepository> _orderRepositoryMock;
+    private readonly Mock<IItemService> _itemServiceMock;
+    private readonly Mock<IUserService> _userServiceMock;
+    private readonly OrderService _orderService;
+    private readonly Fixture _fixture;
+    public OrderServiceTests()
+    {
+        _orderRepositoryMock = new Mock<IOrderRepository>();
+        _itemServiceMock = new Mock<IItemService>();
+        _userServiceMock = new Mock<IUserService>();
+        _orderService = new OrderService(_orderRepositoryMock.Object,
+           _itemServiceMock.Object, _userServiceMock.Object);
+        _fixture = new Fixture();
+    }
 
-    //[Theory]
-    //[AutoData]
-    //public async Task GivenValidDateReturnsList(DateTime date)
-    //{
-    //    var validDate = new DateTime(2012, 1, 1, 12, 0, 0);
-    //    //Arrange 
-    //    _rateRepositoryMock.Setup(x => x.CheckExistence(validDate)).ReturnsAsync(Enumerable.Empty<RateResponse>());
+    [Theory]
+    [AutoData]
+    public async Task Insert_GivenValidRequestReturnsValidResponse(InsertOrderRequest request)
+    {
+        // Arrange
+        InsertItemResponse expectedItemResponse = _fixture.Create<InsertItemResponse>();
+        expectedItemResponse.Id = request.ItemId;
 
-    //    _rateClientMock.Setup(x => x.Get(validDate)).ReturnsAsync(new List<RateEntity>
-    //            {
-    //                           new RateEntity { Currency = "USD", Rate = 1.2m },
-    //                           new RateEntity { Currency = "EUR", Rate = 0.9m }
-    //            });
-    //    //Act
+        UserEntity expectedUserEntity = _fixture.Create<UserEntity>();
+        expectedUserEntity.Id = request.UserId;
 
-    //    var result = await _rateService.Get(validDate);
+        OrderEntity expectedOrderEntity = _fixture.Build<OrderEntity>()
+            .With(orderEntity => orderEntity.ItemId, expectedItemResponse.Id)
+            .With(orderEntity => orderEntity.UserId, expectedUserEntity.Id)
+            .Create();
 
-    //    //Assert
-    ////    Assert.NotEmpty(result);
-    //}
+        _itemServiceMock.Setup(x => x.Get(request.ItemId))
+            .ReturnsAsync(expectedItemResponse);
+
+        _userServiceMock.Setup(x => x.Get(request.UserId))
+            .ReturnsAsync(expectedUserEntity);
+
+        _orderRepositoryMock.Setup(x => x.Insert(It.IsAny<OrderEntity>()))
+            .ReturnsAsync(expectedOrderEntity);
+
+        // Act
+        InsertOrderResponse result = await _orderService.Insert(request);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Id.Should().NotBe(default(int));
+        result.ItemId.Should().Be(expectedItemResponse.Id);
+        result.UserId.Should().Be(expectedUserEntity.Id);
+    }
 }
